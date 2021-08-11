27e280e36439682971b94383fb60aa2dc9a73899
um41r254 <um41r.254@gmail.com>
2021-08-11 17:21:45 +0500

    Initial commit

---
+++ app/src/main/java/com/mid_banchers/practice_with_app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mid_banchers.practice_with_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mid_benchers.practice_with_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
